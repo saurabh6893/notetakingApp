75425f632edee5b18bd468891de5a6afb58bc4f7
Saurabh kedar <saurabhkedar@gmail.com>
2025-09-03 02:53:41 +0530

    corrected cors for the Deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,9 +9,21 @@ import authRouter from "./routes/auth";
 import { connectDB } from "./db";
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://saurabhsnotetakingapp.netlify.app",
+];
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   }),
