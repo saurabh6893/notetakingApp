0c5080fc62dc3b687f396301b4de53607f7b50cc
Saurabh kedar <saurabhkedar@gmail.com>
2025-09-03 06:27:42 +0530

    fix(context): correct AuthProvider import after file split

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { TaskProvider } from "./context/TaskContext";
 import App from "./App";
 import "./App.css";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
