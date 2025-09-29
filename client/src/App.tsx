d7d016037dc3a9f10ec4e9bf10163cd1bcc11ad4
Saurabh kedar <saurabhkedar@gmail.com>
2025-09-29 15:13:05 +0530

    feat(core): implement React code splitting and lazy loading for routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,55 +1,60 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Navbar";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
 import PageTransition from "./Components/PageTransition";
+
+import { Suspense, lazy } from "react";
+const Tasks = lazy(() => import("./Pages/Tasks"));
+const Login = lazy(() => import("./Pages/Login"));
+const Register = lazy(() => import("./Pages/Register"));
+
 import { useAuthStore } from "./stores/useAuthStore";
-import Tasks from "./Pages/Tasks";
 export default function App() {
   const token = useAuthStore((s) => s.token);
   const isAuthenticated = !!token;
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PageTransition>
-              <Home />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/tasks"
-          element={
-            isAuthenticated ? (
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
               <PageTransition>
-                <Tasks />
+                <Home />
               </PageTransition>
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PageTransition>
-              <Login />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PageTransition>
-              <Register />
-            </PageTransition>
-          }
-        />
-      </Routes>
+            }
+          />
+          <Route
+            path="/tasks"
+            element={
+              isAuthenticated ? (
+                <PageTransition>
+                  <Tasks />
+                </PageTransition>
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PageTransition>
+                <Login />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PageTransition>
+                <Register />
+              </PageTransition>
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
