514ce2a5e0a3ef088fc83e1ebe085c7867a9cbb5
Saurabh kedar <saurabhkedar@gmail.com>
2025-11-12 01:22:02 +0530

    feat(auth): add rate limit UI with countdown timer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
 import ErrorBoundary from "./Components/ErrorBoundary";
+import { RateLimitAlert } from "./Components/RateLimitAlert";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
+      <RateLimitAlert />
       <App />
     </ErrorBoundary>
   </StrictMode>,
